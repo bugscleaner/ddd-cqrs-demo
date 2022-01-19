34e74484c0fa3d3cbb95529cfeaa8660635447ef
kenny <kennyuek@gmail.com>
2022-01-19 20:57:36 +0800

    init

--- prduct-center/service/src/main/java/com/cqrs/demo/repository/ProductRepository.java
+++ prduct-center/service/src/main/java/com/cqrs/demo/repository/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.cqrs.demo.repository;
 
+import com.cqrs.demo.domain.ProductDO;
+
 /**
  * @Author: yichen
  * @Date: 2022/1/19 8:41 下午
  */
-public interface ProductRepository {
+public interface ProductRepository extends IRepository<ProductDO>{
 }
